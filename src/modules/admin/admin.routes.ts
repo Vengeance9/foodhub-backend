413d3d02dcfc968fa0ad0f44faa77b76645dab25
vengeance9 <mustakimabtahi207@gmail.com>
2026-02-15 20:23:16 +0600

    modified the  routes

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -1,16 +1,23 @@
 import express, { Router } from "express";
 import { adminController } from "./admin.controller";
-import auth, { UserRole } from "../../middleware.ts/auth";
+import auth, { UserRole } from "../../middleware/auth";
 
 const router = express.Router();
 
-router.get('/users', 
-    auth(UserRole.ADMIN),
-    adminController.getAllUsers)
+router.get("/users", auth(UserRole.ADMIN), adminController.getAllUsers);
 
-router.patch('/users/:id',
-    auth(UserRole.ADMIN),
-    adminController.updateUserRole
-)
+router.patch(
+  "/users/:id",
+  auth(UserRole.ADMIN),
+  adminController.updateUserRole
+);
+
+router.get("/orders", auth(UserRole.ADMIN), adminController.getOrders);
+
+router.put(
+  "/users/:id",
+  auth(UserRole.ADMIN),
+  adminController.updateUserStatus
+);
 
 export const adminRoutes = router;
