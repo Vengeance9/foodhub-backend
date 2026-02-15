6509d5c1f0e5e8eb1d30901bef6a84419ad8f0d8
vengeance9 <mustakimabtahi207@gmail.com>
2026-02-15 20:38:44 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,24 @@ import { providerRoutes } from "./modules/provider/provider.routes";
 import { orderRoutes } from './modules/orders/order.routes';
 import { adminRoutes } from './modules/admin/admin.routes';
 import { reviewRoutes } from './modules/reviews/reviews.routes';
+import { categoryRoutes } from './modules/category/category.routes';
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = process.env.BACKEND_PORT;
+console.log("Better Auth URL:", process.env.BETTER_AUTH_URL);
+console.log("App URL:", process.env.APP_URL);
+console.log("Database URL:", process.env.BACKEND_PORT);
+
+
+
+app.use(
+  cors({
+    origin: `${process.env.APP_URL}`,
+    credentials: true,
+  })
+);
+
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
@@ -16,10 +31,11 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 // or only apply it to routes that don't interact with Better Auth
 app.use(express.json());
 app.use('/provider',providerRoutes)
-app.use('/meals',mealsRoutes)
-app.use('/orders',orderRoutes)
+app.use('/meal',mealsRoutes)
+app.use('/order',orderRoutes)
 app.use('/admin',adminRoutes)
-app.use('/reviews',reviewRoutes)
+app.use('/review',reviewRoutes)
+app.use('/categories',categoryRoutes)
 
 app.listen(port, () => {
   console.log(`Better Auth app listening on port ${port}`);
